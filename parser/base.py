eadf059ad5dad85ea0c8d740e007af18619f5693
beka0708 <abdulasanovbektur@gmail.com>
2023-03-10 20:21:45 +0600

    'finished'

--- parser/base.py
+++ parser/base.py
@@ -29,32 +29,3 @@ for item in table.findAll('a', class_="catalog-list-item"):
 get_cars(cars)
 pp(cars)
 
-
-
-# classwork
-# import requests
-# from bs4 import BeautifulSoup as BS
-# from pprint import pp
-#
-#
-# URL = 'https://mashina.kg'
-# response = requests.get(URL)
-# # print(response.status_code)
-# # print(response)
-# # pp(response)
-# # pp(dir(response))
-# #
-# # print(response.text[:200])
-#
-# soup = BS(response.text, "html.parser")
-# table = soup.find('div', class_="category-block-content")
-# cars = []
-# for obyava in table.findAll('a'):
-#     car = {}
-#     car['name'] = obyava.find('h2'.class_="name")
-#     car['price'] = obyava.find('div', class_="block price").find('strong')
-#
-#     cars.append(car)
-#     print(cars)
-#
-#     # print(obyava.text.strip().replace(' ',''))
